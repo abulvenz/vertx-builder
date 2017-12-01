fddc2884fc57a9b89dabe3a18173ab14ce4ac5fa
Andreas Eismann <andreas@eismaenners.de>
2017-12-01 15:18:32 +0100

    Adding webroot node_modules and other static content

--- src/main/java/de/eismaenners/vertx/builder/ServerVerticle.java
+++ src/main/java/de/eismaenners/vertx/builder/ServerVerticle.java
@@ -7,6 +7,7 @@ package de.eismaenners.vertx.builder;
 
 import io.vertx.core.AbstractVerticle;
 import io.vertx.ext.web.Router;
+import io.vertx.ext.web.handler.StaticHandler;
 
 /**
  *
@@ -14,13 +15,21 @@ import io.vertx.ext.web.Router;
  */
 public class ServerVerticle extends AbstractVerticle {
 
+    public static final String WEBROOT = "src/main/webroot/";
+
     @Override
     public void start() throws Exception {
         Router router = Router.router(vertx);
 
-        router.get("/").handler((ctx) -> {
-            ctx.response().end("Hello vertx builder");
-        });
+        System.out.println("WEBROOT " + System.getProperty("user.dir"));
+
+        router.route("/scripts/*").handler(StaticHandler.create("node_modules"));
+        router.route()
+                .handler(StaticHandler
+                        .create(WEBROOT)
+                        .setCachingEnabled(false)
+                        .setIndexPage("index.html"))
+                .failureHandler(ctx -> ctx.reroute("/"));
 
         vertx.createHttpServer().requestHandler(router::accept).listen(8888);
     }
